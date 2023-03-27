a9fce0096e0554a469f5233a31b6472508a97c79
jz <jiangzhangsg@qq.com>
2023-03-27 15:46:46 +0800

    a version can be use

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use minigrep::{Config, parse_config};
+
+use std::env;
+use std::process;
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config: Config = parse_config(&args);
+
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
+}
+
