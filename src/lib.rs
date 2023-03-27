f5525c60c4049f22a5c82afb0a90a23132a822eb
jz <jiangzhangsg@qq.com>
2023-03-27 16:28:06 +0800

    add env and sensitive

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,47 @@
 use std::fs;
 use std::error::Error;
+use std::env;
 
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub ignore_case: bool,
 }
 
 
 pub fn parse_config(args: &[String]) -> Config {
     let query = args[1].clone();
     let file_path = args[2].clone();
+    let ignore_case = env::var("IGNORE_CASE").is_ok();
 
-    Config {query, file_path}
+    Config {query, file_path, ignore_case}
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
-
-    for line in search(&config.query, &contents) {
+    let results = 
+    if config.ignore_case {
+        search_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+    results.iter().for_each(|line| {
         println!("{}", line);
-    }
-
+    });
     Ok(())
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
+    contents.lines().filter(|line|{
+        line.contains(query)
+    }).collect()
+}
 
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+pub fn search_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    contents.lines().filter(|line|{
+        line.to_ascii_lowercase().contains(&query)
+    }).collect()
 }
 
 // Language: rust
@@ -50,6 +58,6 @@ Pick three.";
     assert_eq!(
         vec!["safe, fast, productive."],
         search(query, contents)
-    );
+        );
 }
 
--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,10 @@ use std::env;
 use std::process;
 fn main() {
     let args: Vec<String> = env::args().collect();
-
     let config: Config = parse_config(&args);
 
     if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
-
         process::exit(1);
     }
 }
-
